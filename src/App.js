ef37b2c0a8fdff125933aa4012911726cda0d1d6
Brindashree C B <brindashree@users.noreply.github.com>
2022-03-06 12:54:47 +0530

    routes fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NotFound from "./challengeOne/NotFound";
 import MyTeam from "./challengeTwo/MyTeam";
 import InteriorConsultant from "./challengeThree/InteriorConsultant";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" exact element={<NotFound />} />
-			<Route path="/my-team" exact element={<MyTeam />} />
-			<Route path="/interior-consult" exact element={<InteriorConsultant />} />
-		</Routes>
+		<Router>
+			<Switch>
+				<Route path="/" exact element={<NotFound />} />
+				<Route path="/my-team" exact element={<MyTeam />} />
+				<Route
+					path="/interior-consult"
+					exact
+					element={<InteriorConsultant />}
+				/>
+			</Switch>
+		</Router>
 	);
 }
 
